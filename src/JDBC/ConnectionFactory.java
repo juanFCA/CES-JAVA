802c70790d9fb13e8269ea2c5a23ce09642d0a15
Juan F Carlos <juanfcarlos.93@gmail.com>
2018-03-16 01:04:41 -0300

    Inicio do Projeto
    Classe ConnectionFactory criada

---
+++ src/JDBC/ConnectionFactory.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alunoces
+ */
+public class ConnectionFactory {
+    public Connection conecta(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost/cesjf","root","ACADEMICO");//ACADEMICO - Senha do Banco de dados
+        } catch (SQLException ex){
+            throw new RuntimeException(ex);
+        }
+    }
+    
+}
