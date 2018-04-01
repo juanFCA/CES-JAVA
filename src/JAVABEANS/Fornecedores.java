f33d2660eae0ec69602924344e67cb83739e191f
JuanFCA <juanfcarlos.93@gmail.com>
2018-04-01 14:51:22 -0300

    Preparação dos Arquivo FornecedorDao Criação do Código Classe que representa o Objeto Fornecedor

--- src/JAVABEANS/Fornecedores.java
+++ src/JAVABEANS/Fornecedores.java
@@ -11,4 +11,91 @@ package JAVABEANS;
  */
 public class Fornecedores {
     
+    private int id;
+    private String nome;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private char uf;
+    private String telefone;
+
+    public Fornecedores() {
+    }
+
+    public Fornecedores(int id, String nome, String rua, int numero, String bairro, String cidade, char uf, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.telefone = telefone;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public char getUf() {
+        return uf;
+    }
+
+    public void setUf(char uf) {
+        this.uf = uf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
 }
